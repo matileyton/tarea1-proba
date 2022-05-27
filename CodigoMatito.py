f87fbb34bcc624fefc675b0fc7b7e6691c818af2
matileyton <maleyton2@miuandes.cl>
2022-05-27 11:49:46 -0400

    p1

--- CodigoMatito.py
+++ CodigoMatito.py
@@ -6,19 +6,11 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     df = pd.read_csv("COVID-19 Coronavirus.csv")
     dCasesForMillion=df.nlargest(20, "Tot Cases//1M pop")
-    casesForCountry=[]
-    countries=[]
-    for country in dCasesForMillion["Country"]:
-        countries.append(country)
-    for casesInCountry in dCasesForMillion["Total Cases"]:
-        casesForCountry.append(casesInCountry)
 
-    plt.bar(np.arange(len(countries)), casesForCountry)
-
-    plt.xticks(np.arange(len(countries)), countries, rotation=80)
+    plt.bar(np.arange(len(dCasesForMillion["Country"].tolist())), dCasesForMillion["Total Cases"].tolist())
+    plt.xticks(np.arange(len(dCasesForMillion["Country"].tolist())), dCasesForMillion["Country"].tolist(), rotation=80)
     plt.xlabel("Paises")
     plt.ylabel("Casos totales de COVID")
     plt.title("Casos totales de COVID por país")
-
     plt.show()
     plt.close
