9cc16dd624028985b444e80906cc5b03d4e0aa4c
Tomas Hevia <thevia@miuandes.cl>
2022-05-29 16:40:48 -0400

    p4

--- CodigoTomas.py
+++ CodigoTomas.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
@@ -19,16 +20,21 @@ archivo = pd.read_csv('COVID-19 Coronavirus.csv')
 archivo = archivo.rename(columns={f'{archivo.columns.values[7]}':'Casos Totales x 1M'})
 archivo = archivo.rename(columns= {f'{archivo.columns.values[8]}':'Muertes Totales x 1M'})
 
-covid = {'Country' : archivo['Country'].tolist() ,
-        'Other names': archivo['Other names'].tolist(),
-        'Code': archivo['ISO 3166-1 alpha-3 CODE'].tolist(),
-        'Population': archivo['Population'].tolist(),
-        'Continent': archivo['Continent'].tolist(),
-        'Total Cases': archivo['Total Cases'].tolist(),
-        'Casos Totales x 1M': archivo['Casos Totales x 1M'].tolist(),
-        'Muertes Totales x 1M': archivo['Muertes Totales x 1M'].tolist(),
-        'Death percentage': archivo['Death percentage'].tolist()}
+#P4
+casos = archivo['Casos Totales x 1M'].tolist()
+paises = archivo['Country'].tolist()
+continente = archivo['Continent'].tolist()
+#print(len(casos), len(paises), len(continente))
+dic=[]
+for x in range(len(casos)):
+    dic.append({'pais':paises[x], 'continente':continente[x],'casos':casos[x] })
+tabla_europa=[]
+tabla_europa.append(['Pais','Casos'])
+for x in dic:
+    
+    if x['continente']== 'Europe':
+        tabla_europa.append([x['pais'] , x['casos']])
 
-print(covid)
-
-
+for i in tabla_europa:
+        print(i[0],i[1])
+    
