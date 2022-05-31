2d89a13514a1cc5ac3140ed9e57f4602109eab77
almandozm <matiasalmandozx@gmail.com>
2022-05-30 21:23:51 -0400

    CodigoAlman

--- CodigoAlman.py
+++ CodigoAlman.py
@@ -3,14 +3,38 @@ import matplotlib.pyplot as plt
 
 #CSV
 archivo = pd.read_csv('COVID-19 Coronavirus.csv')
+archivo = archivo.dropna()
 
 #MODIFICAR NOMBRE COLUMNAS 7 & 8
 archivo = archivo.rename(columns={f'{archivo.columns.values[7]}':'Casos Totales x 1M'})
 archivo = archivo.rename(columns= {f'{archivo.columns.values[8]}':'Muertes Totales x 1M'})
 
 #GRAFICO 2
-archivo.groupby(['Continent'])[['Muertes Totales x 1M']].sum().plot(kind= 'bar', legend= 'Reverse')
+
+muertestotales = archivo['Total Deaths'].tolist()
+continentes = archivo['Continent'].tolist()
+poblacion = archivo['Population'].tolist()
+
+filtrocontinentes = []
+filtromt = {}
+filtropb = {}
+
+for x in range(220):
+    if continentes[x] not in filtrocontinentes:
+        filtromt[f'{continentes[x]}'] = 0
+        filtropb[f'{continentes[x]}'] = 0
+        filtrocontinentes.append(continentes[x])
+
+    filtromt[f'{continentes[x]}'] += muertestotales[x]
+    filtropb[f'{continentes[x]}'] += poblacion[x]
+
+fmhc = []
+for y in filtrocontinentes:
+    fmhc.append(((filtromt[y]/filtropb[y]) * 1000000))
+
+plt.bar(filtrocontinentes, fmhc)
 plt.xlabel('Continentes')
+plt.xticks(rotation = 90)
 plt.ylabel('Cantidad de fallecidos por millon de Habitantes')
 plt.title('GRAFICO 2')
 plt.show()
